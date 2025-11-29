57a668be0da16f4dd02c2e649ff3c6fb838498ce
alexahovhannisyan <alexa.hovhannisyan20@gmail.com>
2025-11-29 18:22:35 +0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("investorBtn").addEventListener("click", function () {
+    window.location.href = "investor.html";
+});
+
+document.getElementById("startuperBtn").addEventListener("click", function () {
+    window.location.href = "startuper.html";
+});
