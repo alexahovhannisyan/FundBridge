1726fbfd1a923ab3c635b1b4fe43f29d8903b194
alexahovhannisyan <alexa.hovhannisyan20@gmail.com>
2025-11-29 19:06:46 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-document.getElementById("investorBtn").addEventListener("click", function () {
-    window.location.href = "investor.html";
+document.getElementById("investorBtn").addEventListener("click", () => {
+  window.location.href = "investor.html";
 });
 
-document.getElementById("startuperBtn").addEventListener("click", function () {
-    window.location.href = "startuper.html";
+document.getElementById("startuperBtn").addEventListener("click", () => {
+  window.location.href = "startuper.html";
 });
